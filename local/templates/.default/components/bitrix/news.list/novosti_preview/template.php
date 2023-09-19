91b4bc5de532ab9cfd6eab40a7b7660894e233d2
Aleksey Fomin <fominalex12@gmail.com>
2023-09-19 18:43:15 +0700

    Возвращение верстки главной страницы (index)novosti preview шаблон

--- local/templates/.default/components/bitrix/news.list/novosti_preview/template.php
+++ local/templates/.default/components/bitrix/news.list/novosti_preview/template.php
@@ -12,37 +12,36 @@
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
 ?>
-<div class="news-list">
-<?if($arParams["DISPLAY_TOP_PAGER"]):?>
-	<?=$arResult["NAV_STRING"]?><br />
-<?endif;?>
-<div class="press-center__articles press-center__articles--wide-list" data-target="view-more.container">
-<?foreach($arResult["ITEMS"] as $arItem):?>
-	<?
-	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
-	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
-	?>
-	<p class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-	<article class="news news--wide">
-      <div class="news__publication-info">
-        <a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="news__link">
-          <h3 class="news__title content-block">
-            <mark>
-			<?=$arItem["NAME"]?>
-            </mark>
-            <span>
-			<?=$arItem["PREVIEW_TEXT"]?>
-			</span>
-          </h3>
-        </a>
-        <time class="news__publication-date" datetime="2019-11-08"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></time>
-      </div>
-      <div class="news__illustration" style="background-image: url(<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>)"></div>
-    </article>
-	</p>
-<?endforeach;?> 
-			</div>
-<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-	<br /><?=$arResult["NAV_STRING"]?>
-<?endif;?>
+<div  class="press-center__articles press-center__articles--dense-list">
+	<?$i=0;?>
+	<?foreach($arResult["ITEMS"] as $arItem):?>
+		<? 
+		$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+		$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+		?>
+		<?if($i==0):?>
+			<article class="news-important" style="background-image: url(<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>)">
+				<pre> <? print_r($arResult["PREVIEW_PICTURE"]["SRC"])?> </pre>
+				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="news-important__link">
+					<h2 class="news-important__title">
+						<? echo $arItem["NAME"];?>
+					</h2>
+				</a>
+				<time class="news-important__publication-date" datetime="<?echo $arItem["DISPLAY_ACTIVE_FROM"]?>"><? echo $arItem["DISPLAY_ACTIVE_FROM"]?></time>
+			</article>
+		<?else:?>
+			<article class="news">
+				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>" class="news__link">
+					<h4 class="news__title">
+						<? echo $arItem["NAME"];?>	
+					</h4>
+				</a>
+				<div class="news__illustration" style="background-image: url(<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>)"></div>
+				<div class="news__publication-info">
+					<time class="news__publication-date" datetime="<?echo $arItem["DISPLAY_ACTIVE_FROM"]?>"><? echo $arItem["DISPLAY_ACTIVE_FROM"]?></time>
+				</div>
+			</article>
+			<?endif?>
+		<?$i++?>
+	<?endforeach;?> 	
 </div>
